d60f079f4deade8be446de1375e8a8032888e99a
Dile95 <727828455@qq.com>
2019-08-20 19:54:51 +0800

    Mall API 模块建立

---
+++ onlinemall_api/src/main/java/com/example/onlinemall_api/OnlinemallApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinemall_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinemallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinemallApiApplication.class, args);
+    }
+
+}
